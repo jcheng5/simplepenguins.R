ef83b1738d89b790cc2eab9ed899d8a4a11730f0
Joe Cheng <joe@posit.co>
2023-04-19 00:32:41 -0700

    Add marginal plots, filtering by species

--- app.R
+++ app.R
@@ -4,14 +4,20 @@ library(dplyr)
 library(ggplot2)
 
 df <- readr::read_csv("penguins.csv")
+species <- unique(df$Species)
 
 ui <- page_fillable(theme = bs_theme(bootswatch = "minty"),
   layout_sidebar(fillable = TRUE,
     sidebar(
       varSelectInput("xvar", "X variable", df, selected = "Bill Length (mm)"),
       varSelectInput("yvar", "Y variable", df, selected = "Bill Depth (mm)"),
+      checkboxGroupInput(
+        "species", "Filter by species", species, selected = species
+      ),
+      hr(),
+      checkboxInput("by_species", "Show species", TRUE),
+      checkboxInput("show_margins", "Show marginal plots", TRUE),
       checkboxInput("smooth", "Add smoother"),
-      checkboxInput("species", "By species", TRUE)
     ),
     plotOutput("scatter")
   )
@@ -19,13 +25,29 @@ ui <- page_fillable(theme = bs_theme(bootswatch = "minty"),
 
 server <- function(input, output, session) {
   subsetted <- reactive({
-    df |> select(!!input$xvar, !!input$yvar, "Species")
+    req(input$species)
+    df |> filter(Species %in% input$species)
   })
   
   output$scatter <- renderPlot({
-    ggplot(subsetted(), aes(!!input$xvar, !!input$yvar)) +
-      geom_point(if (input$species) aes(color=Species)) +
+    p <- ggplot(subsetted(), aes(!!input$xvar, !!input$yvar)) + list(
+      theme(legend.position = "bottom"),
+      if (input$by_species) aes(color=Species),
+      geom_point(),
       if (input$smooth) geom_smooth()
+    )
+
+    if (input$show_margins) {
+      p <- ggExtra::ggMarginal(
+        p,
+        type = if (input$by_species) "density" else "histogram",
+        margins = "both",
+        size = 8,
+        groupColour = input$by_species,
+        groupFill = input$by_species
+      )
+    }
+    p
   }, res = 100)
 }
 
